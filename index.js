cb337275ad627b00e78a63ef951ec4fdced8decb
DavAna <david@anaton.fr>
2018-11-04 22:52:46 +0200

    Cleaned code, used parameters for easier replacement

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 var express = require('express'),
 	app = express();
-app.set('view engine', 'ejs')
-.use(express.static('public'))
 
-.get('/svg', function(req, res){
-	var svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -50 20 100"><g>'
-	for(var i = 0; i < 7; i++){
-		svg+='<path opacity="'+
-			compute.opacity(i)+
-			'"><animate attributeName="d" dur="15s" begin="'
-			+compute.begin(i)+
-			's" repeatCount="indefinite" values="'+
-			compute.animation(i)+
-			'"></animate></path>'
-	}
-	svg += '</g></svg>';
-	res.writeHeader(200, {"Content-Type": "text/html"});  
-	res.write(svg);  
-	res.end()
-})
-
-.listen(8787);
-
+var SETUP = {
+	number_of_waves: 7,
+	color: "#1A50E8",
+	timing: 3, // in seconds, represents the time between 2 successive waves
+}
 var compute = {
 	opacity: function(index){
 		return 1 / (index+1);
 	},
-	animation: function(index){
-		var paths = "";
-		for(var i = 0; i < 3; i++){
-			paths += compute.computeFrame(index, i);
+	timing: function(index){
+		return SETUP.timing * index - SETUP.timing * SETUP.number_of_waves;
+	},
+	animation(index){
+		var av = {
+			starting_point: "-50,0",
+			ending_point: "50,0",
+			cubic_curve_value: (2.5+2.5*(index+1))
 		}
-		return paths;
-	},
-	begin: function(index){
-		return 3 * index - 20;
-	},
-	computeFrame(index, frame){
-		return ["M0,-50 C"+ (2.5+2.5*(index+1)) +",40 "+ (2.5+2.5*(index+1)) +",40 0,50z;",
-				"M0,-50 C"+ (2.5+2.5*(index+1)) +",-40 "+ (2.5+2.5*(index+1)) +",-40 0,50z;",
-				"M0,-50 C"+ (2.5+2.5*(index+1)) +",40 "+ (2.5+2.5*(index+1)) +",40 0,50z;"][frame];
+		var frames = []
+		frames.push([["M", av.starting_point], ["C", "40,", av.cubic_curve_value], ["40,", av.cubic_curve_value], [av.ending_point, 'z']]);
+		frames.push([["M", av.starting_point], ["C", "-40,", av.cubic_curve_value], ["-40,", av.cubic_curve_value], [av.ending_point, 'z']]);
+		frames.push([["M", av.starting_point], ["C", "40,", av.cubic_curve_value], ["40,", av.cubic_curve_value], [av.ending_point, 'z']]);
+		frames = frames.map(frame => frame.map(term => term.join('')).join(' '))
+		return frames.join(';')
 	}
 }
+
+app.set('view engine', 'ejs')
+.use(express.static('public'))
+
+.get('/svg', function(req, res){
+	var paths = []
+	for(var i = 0; i < SETUP.number_of_waves; i++){
+		paths.push({
+			opacity: compute.opacity(i),
+			animation: compute.animation(i),
+			timing: compute.timing(i),
+			frames: compute.animation(i),
+		})
+	}
+	res.render('svg', {setup: SETUP, paths: paths})
+})
+
+.listen(8787);
